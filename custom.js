47ef9b25b325934587a27c23ab3f2500bfda5bcf
Christiazz <109076764+Christiazz@users.noreply.github.com>
2022-12-15 12:37:17 +0100

    asedsd

--- custom.js
+++ custom.js
@@ -1,4 +1,4 @@
-const linkEng = document.querySelectorAll('.weglot-link-en');
-const linkIta = document.querySelectorAll('.weglot-link-it');
+const linkEng = document.getElementsByClassName('weglot-link-en');
+const linkIta = document.getElementsByClassName('weglot-link-it');
 
 console.log(linkEng + linkIta);
