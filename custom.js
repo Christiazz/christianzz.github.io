25e1b60451d6292a68e195debb0237af6a861555
Christiazz <109076764+Christiazz@users.noreply.github.com>
2022-12-15 12:28:48 +0100

    link lang intervention

--- custom.js
+++ custom.js
@@ -1,1 +1,4 @@
-console.log('script avviato');
+const linkEng = document.querySelectorAll('.weglot-link-en');
+const linkIta = document.querySelectorAll('.weglot-link-it');
+
+console.log(linkEng, linkIta);
