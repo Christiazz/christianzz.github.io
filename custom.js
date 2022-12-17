e5c9f2a806711efa5103bec71346b9aff2de2e39
Christiazz <109076764+Christiazz@users.noreply.github.com>
2022-12-17 10:49:10 +0100

    sd

--- custom.js
+++ custom.js
@@ -2,6 +2,8 @@ const linkEng = document.getElementsByClassName('weglot-link-en');
 const linkIta = document.getElementsByClassName('weglot-link-it');
 const LangIndicator = document.getElementsByClassName('lang-indicator');
 
+console.log(linkIta);
+
 function linkIndicator() {
   if (linkIta.classList.contains('weglot-link--active')) {
     LangIndicator.classList.add('active-lang-indicator');
