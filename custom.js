955870707f6581e7a27018453ecc90d3ec3eda6f
Christiazz <109076764+Christiazz@users.noreply.github.com>
2022-12-17 11:32:35 +0100

    ww

--- custom.js
+++ custom.js
@@ -3,9 +3,15 @@ const linkIta = document.getElementsByClassName('weglot-link-it');
 const LangIndicator = document.getElementById('lang-dicator');
 
 console.log(linkIta);
+console.log(linkEng);
+console.log(LangIndicator);
 
 if (linkIta.classList.contains('weglot-link--active')) {
   LangIndicator.classList.add('active');
 } else if (linkEng.classList.contains('weglot-link--active')) {
   LangIndicator.classList.add('de-active');
 }
+
+console.log(linkIta);
+console.log(linkEng);
+console.log(LangIndicator);
