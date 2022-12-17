1d346965ac701f984ea7a290cd121e25a09742e7
Christiazz <109076764+Christiazz@users.noreply.github.com>
2022-12-17 11:44:39 +0100

    eee

--- custom.js
+++ custom.js
@@ -6,6 +6,17 @@ console.log(linkIta);
 console.log(linkEng);
 console.log(LangIndicator);
 
+function noAddActive() {
+  linkEng.classList.add('de-active');
+}
+
+function addActive() {
+  linkIta.classList.add('active');
+}
+
+linkEng.addEventListener('click', noAddActive);
+linkIta.addEventListener('click', addActive);
+
 if (linkIta.classList.contains('weglot-link--active')) {
   LangIndicator.classList.add('active');
 } else if (linkEng.classList.contains('weglot-link--active')) {
