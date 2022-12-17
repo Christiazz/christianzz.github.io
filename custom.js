ebbd75ed166aff781479413058ce1ee5fe89369f
Christiazz <109076764+Christiazz@users.noreply.github.com>
2022-12-17 11:02:58 +0100

    ss

--- custom.js
+++ custom.js
@@ -1,14 +1,14 @@
 const linkEng = document.getElementsByClassName('weglot-link-en');
 const linkIta = document.getElementsByClassName('weglot-link-it');
-const LangIndicator = document.getElementsByClassName('lang-indicator');
+const LangIndicator = document.getElementById('lang-dicator');
 
 console.log(linkIta);
 
 function linkIndicator() {
   if (linkIta.classList.contains('weglot-link--active')) {
-    LangIndicator.classList.add('active-lang-indicator');
+    LangIndicator.classList.add('active');
   } else if (linkEng.classList.contains('weglot-link--active')) {
-    LangIndicator.classList.add('de-active-lang-indicator');
+    LangIndicator.classList.add('de-active');
   }
 }
 
