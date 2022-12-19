decf0f392d416629d63da2cc3247302a4060d6ad
Christiazz <109076764+Christiazz@users.noreply.github.com>
2022-12-19 19:20:43 +0100

    s

--- custom.js
+++ custom.js
@@ -19,3 +19,23 @@ linkEng.addEventListener('click', noAddActive);
 linkIta.addEventListener('click', addActive);
 
 // logica lang-indicator solo per lingue mobile
+
+const linkEng = document.getElementById('link-eng-offc');
+const linkIta = document.getElementById('link-ita-offc');
+const LangIndicator = document.getElementById('lang-dicator-offc');
+
+if (linkIta.classList.contains('active')) {
+  LangIndicator.classList.add('active-state');
+}
+
+function noAddActive() {
+  linkEng.classList.add('de-active');
+}
+
+function addActive() {
+  linkIta.classList.add('active');
+  LangIndicator.classList.add('active');
+}
+
+linkEng.addEventListener('click', noAddActive);
+linkIta.addEventListener('click', addActive);
