695b77c5ea07b68122ba456bfda4b3c8ad15d319
Christiazz <109076764+Christiazz@users.noreply.github.com>
2022-12-20 14:39:08 +0100

    f

--- custom.js
+++ custom.js
@@ -57,54 +57,20 @@ function trigOff() {
 
 closeOffc.addEventListener('click', trigOff);
 
-// CAROSELLO
+/* bloccare lo scroll del body quando l'offcanvas è attivo */
 
-/*
+const menuOffcanvas = document.querySelector('.offcanvas');
+var body = document.querySelector('body');
+var menuOffcanvasToggle = document.querySelector('.offcanvas-toggle-menu');
 
-function carousel() {
-  let carouselSlider = document.querySelector(".carousel__slider");
-  let list = document.querySelector(".carousel__list");
-  let item = document.querySelectorAll(".carousel__item");
-  let list2;
-
-  const speed = 1;
-
-  const width = list.offsetWidth;
-  let x = 0;
-  let x2 = width;
-
-  function clone() {
-    list2 = list.cloneNode(true);
-    carouselSlider.appendChild(list2);
-    list2.style.left = `${width}px`;
-  }
-
-  function moveFirst() {
-    x = x - speed;
-
-    if (width >= Math.abs(x)) {
-      list.style.left = `${x}px`;
-    } else {
-      x = width;
-    }
-  }
-
-  function moveSecond() {
-    x2 -= speed;
-
-    if (list2.offsetWidth >= Math.abs(x2)) {
-      list2.style.left = `${x2}px`;
-    } else {
-      x2 = width;
-    }
-  }
-
-  clone();
-
-  let a = setInterval(moveFirst, 10);
-  let b = setInterval(moveSecond, 10);
+function toggleOffcanvas() {
+  menuOffcanvas.classList.add('active');
 }
 
-carousel();
+menuOffcanvasToggle.addEventListener('click', toggleOffcanvas);
 
-*/
+if (menuOffcanvas.classList.contains('active')) {
+  body.style.overflowY = 'hidden';
+} else {
+  return;
+}
