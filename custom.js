89556604d9b7f6dfec092552461185a8ba30f1c5
Christiazz <109076764+Christiazz@users.noreply.github.com>
2022-12-20 21:49:53 +0100

    s

--- custom.js
+++ custom.js
@@ -52,7 +52,7 @@ function trigOff() {
   setTimeout(function () {
     closeUno.classList.remove('close-active-sei');
     closeDue.classList.remove('close-active-sette');
-  }, 2000);
+  }, 1500);
 }
 
 closeOffc.addEventListener('click', trigOff);
